164f0560b60f2f0f309531d61ac908fa8c7c6362
Digvijay <digvijaybakare816@gmail.com>
2024-11-07 11:01:18 +0530

    Modified string programs

--- String/NoOfVCAndS.java
+++ String/NoOfVCAndS.java
@@ -10,12 +10,13 @@ public class NoOfVCAndS {
         String string = "What is the meaning of this!!";
         string = string.toLowerCase();
         System.out.println("Original string: " + string);
+        String vowels = "aeiouAEIOU";
 
         // counting the vowels, consonants and spaces
         int vowel = 0;
         int space = 0;
         int consonant = 0;
-        int sChar =0;
+        int sChar = 0;
 
         for (int i = 0; i < string.length(); i++) {
             if (string.charAt(i) == 'a' || string.charAt(i) == 'e' || string.charAt(i) == 'i' || string.charAt(i) == 'o'
@@ -30,7 +31,8 @@ public class NoOfVCAndS {
                 consonant++;
             }
         }
+
         System.out.println("The number of vowels, consonants and spaces in the given string: " +
-                "\nVowels: " + vowel + "\nConsonants: " + consonant + "\nSpaces: " + space+"\nSpecial Characters: " + sChar);
+                "\nVowels: " + vowel + "\nConsonants: " + consonant + "\nSpaces: " + space + "\nSpecial Characters: " + sChar);
     }
 }
--- String/RemoveVowels.java
+++ String/RemoveVowels.java
@@ -3,8 +3,32 @@ package TCS_NQT.String;
 // Que: Remove all vowels from the string
 
 public class RemoveVowels {
+    public static String removeVowels(String str) {
+        // Using StringBuilder for efficient string manipulation
+        StringBuilder sb = new StringBuilder();
+
+        // Define vowels (both uppercase and lowercase)
+        String vowels = "aeiouAEIOU";
+
+        // Iterate through each character in the string
+        for (char c : str.toCharArray()) {
+            // If the character is not a vowel, append it to the StringBuilder
+            if (vowels.indexOf(c) == -1) {
+                sb.append(c);
+            }
+        }
+
+        // Convert StringBuilder back to String
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         // declare and print a string
         String str = "Remove all vowels";
+        System.out.println("Original string: " + str);
+
+        // remove all the vowels
+        String result = removeVowels(str);
+        System.out.println("String after removing vowels: " + result);
     }
 }
